2d5fd2ee754c4aeca5dea7be075a229bbf7d99eb
ppaka <asj0216@gmail.com>
2023-03-27 21:30:46 +0900

    테스트 스크립트 생성

---
+++ Assets/Scripts/Game/Yarn/YarnEffectController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Yarn.Unity;
+
+namespace Game.Yarn
+{
+    public class YarnEffectController : MonoBehaviour
+    {
+        [YarnCommand("camera_look")]
+        private void CameraLookAtTarget(GameObject target)
+        {
+            if (target == null) {
+                Debug.Log("Can't find the target!");
+            }
+            
+            // Make the main camera look at this target
+            if (UnityEngine.Camera.main != null) UnityEngine.Camera.main.transform.LookAt(target.transform);
+            else Debug.LogError("Can't find the MainCamera!");
+        }    
+    }
+}
