828c5f9c60c7bf7df30765e7d152dc90485589a0
ppaka <asj0216@gmail.com>
2023-07-25 13:40:57 +0900

    Dialogue View 생성

---
+++ Assets/Scripts/Game/Yarn/NormalDialogueView.cs
@@ -0,0 +1,14 @@
+﻿using Yarn.Unity;
+
+namespace Game.Yarn
+{
+    public class NormalDialogueView : DialogueViewBase
+    {
+        public LineView view;
+        
+        public void Interrupt()
+        {
+            view.currentStopToken.Interrupt();
+        }
+    }
+}
